bdf9b96e5d9a4d4f05b466973d70ee053dc61a7a
mibarg <mibarg@users.noreply.github.com>
2019-10-24 21:41:21 +0300

    Exercise six - log_mul

--- Theorem Proving in Lean/4.quantifiers_and_equality.lean
+++ Theorem Proving in Lean/4.quantifiers_and_equality.lean
@@ -373,3 +373,38 @@ namespace five
         exists.intro a (false_imp_any _ _ nr)))
         
 end five
+
+namespace six
+    /-
+    Give a calculational proof of the theorem log_mul below.
+    -/
+
+    variables (real : Type) [ordered_ring real]
+    variables (log exp : real → real)
+    variable  log_exp_eq : ∀ x, log (exp x) = x
+    variable  exp_log_eq : ∀ {x}, x > 0 → exp (log x) = x
+    variable  exp_pos    : ∀ x, exp x > 0
+    variable  exp_add    : ∀ x y, exp (x + y) = exp x * exp y
+
+    -- this ensures the assumptions are available in tactic proofs
+    include log_exp_eq exp_log_eq exp_pos exp_add
+
+    example (x y z : real) :
+    exp (x + y + z) = exp x * exp y * exp z :=
+    by rw [exp_add, exp_add]
+
+    example (y : real) (h : y > 0)  : exp (log y) = y :=
+    exp_log_eq h
+
+    example {x y : real} (hx : x > 0) (hy : y > 0) : exp (log x + log y) = x * y
+    := calc
+    exp (log x + log y) = exp (log x) * exp (log y) : by rw exp_add
+    ... = x * y : by rw [exp_log_eq hx, exp_log_eq hy]
+
+    theorem log_mul {x y : real} (hx : x > 0) (hy : y > 0) :
+    log (x * y) = log x + log y :=
+    calc
+    log (x * y) = log (exp (log x) * exp (log y)) : by rw [exp_log_eq hx, exp_log_eq hy]
+    ... = log (exp (log x + log y)) : by rw exp_add
+    ... = log x + log y : by rw log_exp_eq
+end six
